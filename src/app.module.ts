836bba156318bb0bb289d81fde33f3889b31e217
Uy Seng <senguy44056@gmail.com>
2023-03-16 11:56:39 -0500

    init auth and user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -8,6 +10,8 @@ import { PrismaModule } from './prisma/prisma.module';
       isGlobal: true,
     }),
     PrismaModule,
+    AuthModule,
+    UserModule,
   ],
 })
 export class AppModule {}
