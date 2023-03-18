343e5da73521f82e337bdc41058defec838ee1bb
Uy Seng <senguy44056@gmail.com>
2023-03-18 12:07:04 -0500

    add jwt strategy and update jwt guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserService } from '../user/user.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy';
 
 @Module({
+  imports: [JwtModule.register({ secret: process.env.JWT_SECRET })],
   controllers: [AuthController],
-  providers: [AuthService, UserService],
+  providers: [AuthService, UserService, JwtStrategy],
 })
 export class AuthModule {}
---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          const data = request?.cookies['access_token'];
+          if (!data) {
+            return null;
+          }
+          return data;
+        },
+      ]),
+      secretOrKey: configService.get('JWT_SECRET'),
+      ignoreExpiration: false,
+    });
+  }
+
+  async validate(payload: any) {
+    if (payload === null) {
+      throw new UnauthorizedException();
+    }
+    return payload;
+  }
+}
